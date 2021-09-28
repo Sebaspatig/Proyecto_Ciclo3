eafddfa6e4e394086bdba62f9ef88f58e04da6c7
Sebaspatig <sebaspatig@gmail.com>
2021-09-28 13:57:33 -0500

    Se hacen pequeñas modificaciondes de funcionamiento

--- js/registro.js
+++ js/registro.js
@@ -3,12 +3,7 @@ let usercount=0;
 
 /**
  * Este es el contructor que permitira la creacion del objeto usuario
- * @param {*} id 
- * @param {*} tipo_documento 
- * @param {*} numero_documento 
- * @param {*} correo 
- * @param {*} contrasena 
- */
+*/
 function Registro(id,tipo_documento,numero_documento,correo,contrasena){
     this.id=id;
     this.tipo_documento=tipo_documento;
@@ -18,17 +13,15 @@ function Registro(id,tipo_documento,numero_documento,correo,contrasena){
 
 }
 
-
-/**
- * Esta funcion crea el arreglo de objetos de los registros que se van agregando
- */
-function agregarRegistro(/*tipo_doc,num_doc,correo*/){
-
+/* Esta funcion crea el arreglo de objetos de los registros que se van agregando
+*/
+function agregarRegistro(tipo_doc,num_doc,correo,contrasena){
     
-    var tipo_doc=document.getElementById("tipo_documento").value;
-    var num_doc=document.getElementById("numero_documento").value;
-    var correo=document.getElementById("correo").value;
-    var contrasena=document.getElementById("contrasena").value;
+    
+    // var tipo_doc=document.getElementById("tipo_documento").value;
+    // var num_doc=document.getElementById("numero_documento").value;
+    // var correo=document.getElementById("correo").value;
+    // var contrasena=document.getElementById("contrasena").value;
     
     var usuario=new Registro(usercount,tipo_doc,num_doc,correo,contrasena);
 
@@ -36,8 +29,6 @@ function agregarRegistro(/*tipo_doc,num_doc,correo*/){
 
     usercount+=1;
     users.push(usuario);
-   
-
 }
 
 /**
@@ -58,9 +49,7 @@ function obtenerRegPasaporte(arreglo){
  * @returns el arreglo ordenado
  */
 function ordenarArreglo(arreglo){
-    ordenamiento = {pasaporte: 1, cedula: 2, taridentidad: 3, regcivil: 4, cedulaextra: 5};
-
-    var ordenado= arreglo.sort(function (o1,o2) {
+    return arreglo.sort(function (o1,o2) {
         if (o1.numero_documento > o2.numero_documento) { //comparación lexicogŕafica
             return 1;
         } else if (o1.numero_documento < o2.numero_documento) {
@@ -68,32 +57,46 @@ function ordenarArreglo(arreglo){
         } 
         return 0;
     }); 
-
-    return ordenado.sort((
-        (a, b) => ordenamiento[a.tipo_documento] - ordenamiento[b.tipo_documento])
-      );
 }
 
+function ordenarArreglo2(arreglo){
+    ordenamiento = {pasaporte: 1, cedula: 2, taridentidad: 3, regcivil: 4, cedulaextra: 5};
+    
+    var ordenado= arreglo.sort(function (o1,o2) {
+        if (o1.numero_documento > o2.numero_documento) { //comparación lexicogŕafica
+           return 1;
+       } else if (o1.numero_documento < o2.numero_documento) {
+           return -1;
+       } 
+       return 0;
+   }); 
+   
+   return ordenado.sort((
+       (a, b) => ordenamiento[a.tipo_documento] - ordenamiento[b.tipo_documento])
+       );
+   }
+
 /*Modulo exports*/
+                
 // module.exports={obtenerRegPasaporte,ordenarArreglo}
+                
+function filtrar(){
+    console.log(obtenerRegPasaporte(users))
+    console.table(ordenarArreglo(users));
+    // console.table(ordenarArreglo(obtenerRegPasaporte(users)));
 
-// function filtrar(){
-//     // console.log(obtenerRegPasaporte(users))
-//     // console.table(ordenarArreglo(obtenerRegPasaporte(users)));
-//     console.table(ordenarArreglo(users));
-
-// }
-
+}
 
 /** Valores de prueba */
-agregarRegistro("pasaporte", "AD123459", "sllower@email.com")
-agregarRegistro("cedula", "1037600365", "sllower@gmail.com")
-agregarRegistro("pasaporte", "AS123451", "sllower@email.com")
-agregarRegistro("taridentidad", "1128449711", "sllower@gmail.com")
-agregarRegistro("pasaporte", "AS123431", "sllower@email.com")
-agregarRegistro("cedula", "1234567854", "sllower@mail.com")
-agregarRegistro("pasaporte", "AS123232", "sllower@hotmail.com")
+agregarRegistro("pasaporte", "AS123459", "prueba1@email.com","pass1234")
+agregarRegistro("cedula", "1137600365", "prueba2@gmail.com","pass1234")
+agregarRegistro("pasaporte", "AD123451", "prueba3@email.com","pass1234")
+agregarRegistro("taridentidad", "1128449711", "prueba4@gmail.com","pass1234")
+agregarRegistro("pasaporte", "AO123431", "prueba5@email.com","pass1234")
+agregarRegistro("cedula", "1034567854", "prueba6@mail.com","pass1234")
+agregarRegistro("pasaporte", "AS023232", "prueba7@hotmail.com","pass1234")
 
-const orlist = ordenarArreglo(users)
-console.log(orlist)
+console.table(obtenerRegPasaporte(users))
+console.table(ordenarArreglo(obtenerRegPasaporte(users)));
+console.table(ordenarArreglo2(users));
 
