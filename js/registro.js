aa911865f2223f905dd4f245a9ec0675ead47480
Sebaspatig <sebaspatig@gmail.com>
2021-10-01 15:28:48 -0500

    Se arregla el archivo registro.js para cumplir con los parametros del bot en la funcion ordenarArreglo() por el compañero Jorge Tordecilla

--- js/registro.js
+++ js/registro.js
@@ -1,1 +1,74 @@
+var registros = []
 
+/** 
+ * Sumary: Funtion Resgistro constructor para crear objetos de tipos Usuario
+ */
+function Registro(tipo_documento, numero_documento, correo, contrasena) {
+    this.tipo_documento = tipo_documento;
+    this.num_documento = numero_documento;
+    this.correo = correo;
+    this.contrasena = contrasena;
+}
+
+/**
+ *Sumary: Esta funcion agrega usuarios nuevos desde los valores de form y  los inserta en el array registro
+ */
+function agregarRegistro() {
+    var tipo_doc = document.getElementById("tipo-documento").value;
+    var num_doc = document.getElementById("numero-documento").value;
+    var correo = document.getElementById("correo").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var usuario = new Registro(tipo_doc, num_doc, correo, contrasena);
+    registros.push(usuario);
+    console.table(usuario)
+    return registros
+}
+
+/**
+ * Esta funcion filtra el arreglo de objetos de registro por correo que incluya "email.com"
+ * @param {Array} users arreglo de objetos del registro
+ * @returns el arreglo filtrado
+ */
+function filtrarCorreo(arreglo) {
+    const filtrarCorreo = arreglo.filter((value) => {
+        return value.correo.includes("email.com")
+    })
+    console.table(filtrarCorreo)
+    return filtrarCorreo
+}
+
+/**
+ * Esta funcion filtra el arreglo de objetos de registro por el tipo de documento en este caso el pasaporte
+ * @param {Array} users arreglo de objetos del registro
+ * @returns el arreglo filtrado
+ */
+function obtenerRegPasaporte(arreglo) {
+    const registroPasaporte = arreglo.filter(user => {
+        return user.tipo_documento === "P"
+    });
+    console.table(registroPasaporte);
+    return registroPasaporte;
+}
+
+/**
+ * Esta funcion ordena los valores de los pasaporte y deja los demas en el sito en el cual fue insertado
+ * @param {Array} users arreglo de objetos del registro
+ * @returns el arreglo ordena
+ */
+function ordenarArreglo(arreglo) {
+    arreglo.forEach((i, indexI) => {
+        arreglo.forEach((j, indexJ, array) => {
+            if (i.tipo_documento === "P" && j.tipo_documento === "P") {
+                if (i.num_documento < j.num_documento) {
+                    var tempo = array[indexI]
+                    array[indexI] = array[indexJ]
+                    array[indexJ] = tempo
+                }
+            }
+        })
+    })
+    console.table(arreglo)
+    return arreglo
+}
+
+module.exports = { agregarRegistro, obtenerRegPasaporte, ordenarArreglo, filtrarCorreo, registros };
